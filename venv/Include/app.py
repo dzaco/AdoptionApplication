7002ded326cca59cbcf681a589c627ec337858d7
Jacek Giedronowicz <jacek.giedronowicz@gmail.com>
2019-12-28 16:36:24 +0100

    app and nav

--- venv/Include/app.py
+++ venv/Include/app.py
@@ -2,5 +2,8 @@ from flask import Flask
 
 app = Flask(__name__, instance_relative_config=False)
 
+from Controller.nav import main
+app.register_blueprint(main)
+
 if __name__ == '__main__':
     app.run(debug=True)
