4384c93ceda75b2a8b36f28d6a9338cfcfae5d56
Jacek Giedronowicz <jacek.giedronowicz@gmail.com>
2019-12-30 13:04:05 +0100

    form

--- venv/Include/Controller/nav.py
+++ venv/Include/Controller/nav.py
@@ -6,3 +6,13 @@ main = Blueprint('main' , __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/adopt', methods = ['POST'])
+def adopt():
+    data = request.form
+    print(data)
+    return render_template('adopt.html', data=data)
+
+@main.route('/test')
+def test():
+    return render_template('test.html')
